6d574cb53ce252f5800c8225df923122f2a5843f
cding1 <clarabding@gmail.com>
2019-05-07 10:47:48 +0000

    add styling to mood page

--- client/src/js/pages/moodTrackingPage/moodTrackingPage.js
+++ client/src/js/pages/moodTrackingPage/moodTrackingPage.js
@@ -3,6 +3,17 @@ const BasePage = require('watch-framework').BasePage;
 class MoodTrackingPage extends BasePage {
   template = require('./moodTrackingPage.hbs');
 
+ pageWillLoad() {
+    const home = require(`../../../images/home.png`)
+    const left = require(`../../../images/left.png`)
+    const right = require(`../../../images/right.png`)
+
+    this.home = home
+    this.left = left
+    this.right = right
+    }
+
+
    rightButtonEvent() {
     this.navigate('happyMoodPage');
   }
